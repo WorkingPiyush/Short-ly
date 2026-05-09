769a1a79201a3ef899c21b714304e7a2db4cad0d
WorkingPiyush <itsmepiyush855@gmail.com>
2026-05-10 00:49:54 +0530

    feat: improve login and signup UI with animations

--- FRONTEND/src/Pages/Auth/Login.jsx
+++ FRONTEND/src/Pages/Auth/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Eye, EyeOff } from "lucide-react";
 import { Link } from 'react-router-dom';
+import FadeUp from '../../animation/framer-motion';
 const googleIcon = "/icons8-google.svg";
 const githubIcon = "/github-142-svgrepo-com.svg";
 
@@ -8,81 +9,85 @@ function Login() {
     const [showPassword, setShowPassword] = useState(false);
     return (
         <div className="min-h-screen bg-black flex flex-col items-center justify-center pt-20">
+            <FadeUp>
+                <div className='w-full flex flex-col items-center gap-3 py-5 md:py-10'>
+                    <h1 className='font-bold text-2xl md:text-4xl'>Welcome back buddy !.😄</h1>
+                    <div><span className='text-zinc-500 tracking-wider'>Don&apos;t have an account?</span><Link className='p-2 text-emerald-300' to="/signup">Sign up free</Link></div>
+                </div>
+            </FadeUp>
 
-            <div className='w-full max-w-xl flex flex-col gap-3 py-5 md:py-10'>
-                <h1 className='font-bold text-3xl md:text-4xl'>Welcome back buddy !.😄</h1>
-                <div><span className='text-zinc-500 tracking-wider'>Don&apos;t have an account?</span><Link className='p-2 text-emerald-300' to="/signup">Sign up free</Link></div>
-            </div>
+            <FadeUp>
+                <div className="w-full max-w-xl rounded-3xl border border-emerald-500/10 bg-[#0b1110] p-10 shadow-xl">
+                    <form className="space-y-6">
 
-            <div className="w-full max-w-xl rounded-3xl border border-emerald-500/10 bg-[#0b1110] p-10 shadow-xl">
-                <form className="space-y-6">
+                        {/* Email */}
+                        <div>
+                            <label className="block text-zinc-400 text-sm mb-2">
+                                Email address
+                            </label>
 
-                    {/* Email */}
-                    <div>
-                        <label className="block text-zinc-400 text-sm mb-2">
-                            Email address
-                        </label>
-
-                        <input
-                            type="email"
-                            placeholder="you@example.com"
-                            className="w-full rounded-xl border border-zinc-700 
+                            <input
+                                type="email"
+                                placeholder="you@example.com"
+                                className="w-full rounded-xl border border-zinc-700 
                          bg-zinc-900/80 px-5 py-4 text-white
                          placeholder:text-zinc-400
                          outline-none focus:border-emerald-500/40
                          transition"
-                        />
-                    </div>
+                            />
+                        </div>
 
-                    {/* Password */}
-                    <div>
-                        <label className="block text-zinc-400 text-sm mb-2">
-                            Password
-                        </label>
+                        {/* Password */}
+                        <div>
+                            <label className="block text-zinc-400 text-sm mb-2">
+                                Password
+                            </label>
 
-                        <div className="relative">
+                            <div className="relative">
 
-                            <input
-                                type={`${showPassword ? "text" : "password"}`}
-                                placeholder="Create a strong password"
-                                className="w-full rounded-xl border border-zinc-700 
+                                <input
+                                    type={`${showPassword ? "text" : "password"}`}
+                                    placeholder="Create a strong password"
+                                    className="w-full rounded-xl border border-zinc-700 
                            bg-zinc-900/80 px-5 py-4 pr-16 text-white
                            placeholder:text-zinc-400
                            outline-none focus:border-emerald-500/40
                            transition"
-                            />
+                                />
 
-                            {/* Eye button */}
-                            <button
-                                type="button"
-                                className="absolute right-2 top-1/2 -translate-y-1/2
+                                {/* Eye button */}
+                                <button
+                                    type="button"
+                                    className="absolute right-2 top-1/2 -translate-y-1/2
                            h-12 w-12 rounded-xl border border-zinc-700
                            flex items-center justify-center
                            text-zinc-400 hover:text-white transition"
-                            >
-                                {showPassword ? <EyeOff onClick={() => setShowPassword(false)} size={20} /> : <Eye onClick={() => setShowPassword(true)} size={20} />}
-                            </button>
+                                >
+                                    {showPassword ? <EyeOff onClick={() => setShowPassword(false)} size={20} /> : <Eye onClick={() => setShowPassword(true)} size={20} />}
+                                </button>
+                            </div>
+                            <div className='flex justify-end'>
+                                <span className='text-sm text-zinc-500'>Forgot Password ?</span>
+                            </div>
                         </div>
-                        <div className='flex justify-end'>
-                            <span className='text-sm text-zinc-500'>Forgot Password ?</span>
+                    </form>
+                    <div className='p-4'>
+                        <button className='py-2 w-full border cursor-pointer border-zinc-400 shadow rounded-xl hover:scale-95 transition-all'>Login to Short-ly</button>
+                    </div>
+                    <div>
+                        <div className='flex items-center gap-2'>
+                            <span className='h-px flex-1 bg-zinc-800'></span>
+                            <span className='text-sm text-zinc-500 font-medium whitespace-nowrap'>or continue with</span>
+                            <span className='h-px flex-1 bg-zinc-800'></span>
                         </div>
                     </div>
-                </form>
-                <div className='p-4'>
-                    <button className='py-2 w-full border cursor-pointer border-zinc-400 shadow rounded-xl hover:scale-95 transition-all'>Login to Short-ly</button>
-                </div>
-                <div>
-                    <div className='flex items-center gap-2'>
-                        <span className='h-px flex-1 bg-zinc-800'></span>
-                        <span className='text-sm text-zinc-500 font-medium whitespace-nowrap'>or continue with</span>
-                        <span className='h-px flex-1 bg-zinc-800'></span>
+                    <div className='flex flex-col md:flex-row gap-4 items-center justify-center p-4'>
+                        <button className='py-2 px-20 border border-zinc-400 flex gap-2 justify-center items-center shadow rounded-xl cursor-pointer hover:scale-102 transition-all'><img className='h-8 w-8' src={googleIcon} alt="Google" /> Google</button>
+                        <button className='py-2 px-20 border border-zinc-400 flex gap-2 justify-center items-center shadow rounded-xl cursor-pointer hover:scale-102 transition-all'><img className='h-8 w-8 bg-white rounded-full' src={githubIcon} alt="github" />GitHub</button>
                     </div>
                 </div>
-                <div className='flex flex-col md:flex-row gap-4 items-center justify-center p-4'>
-                    <button className='py-2 px-20 border border-zinc-400 flex gap-2 justify-center items-center shadow rounded-xl cursor-pointer hover:scale-102 transition-all'><img className='h-8 w-8' src={googleIcon} alt="Google" /> Google</button>
-                    <button className='py-2 px-20 border border-zinc-400 flex gap-2 justify-center items-center shadow rounded-xl cursor-pointer hover:scale-102 transition-all'><img className='h-8 w-8 bg-white rounded-full' src={githubIcon} alt="github" />GitHub</button>
-                </div>
-            </div>
+            </FadeUp>
+
         </div>
     )
 }
