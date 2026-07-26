81a5a6bb7ee6344d83f11a61de7544c7c2dcbb41
WorkingPiyush <itsmepiyush855@gmail.com>
2026-07-26 17:38:27 +0530

    fix: whitelist frontend branch URL

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_BRANCH_URL],
     credentials: true,
 }));
 app.use(passport.initialize());
