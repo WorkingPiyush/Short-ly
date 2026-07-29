34c7eaf435a0199315fcb35c310eb2a053ac1ec3
WorkingPiyush <itsmepiyush855@gmail.com>
2026-07-30 00:34:23 +0530

    fix: update Google OAuth redirect URL

--- BACKEND/config/passport.auth.js
+++ BACKEND/config/passport.auth.js
@@ -7,7 +7,7 @@ import { handleOAuthUser } from '../src/modules/Auth/service.js';
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `${process.env.REDIRECT_URL}/api/${process.env.GOOGLE_CALLBACK_URL}`,
+    callbackURL: `${process.env.BACKEND_URL}/api/${process.env.GOOGLE_CALLBACK_URL}`,
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         const name = profile.displayName || profile.name.givenName;
